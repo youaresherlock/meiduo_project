cb5eb41de9369b7fbd8aea5bc4400e3e98de028d
clarence <2263194561@qq.com>
2020-06-26 21:38:10 +0800

    配置redis

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -125,3 +125,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# redis配置
+CACHES = {
+    "default": {  # 默认的缓存配置
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": {  # 希望将session存储在redis的1号库
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+# Django默认的配置,存储在数据库中
+# SESSION_ENGINE = "django.contrib.sessions.backends.db"
+# 现在修改redis存储session
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# 存储session数据时使用的配置别名
+# SESSION_CACHE_ALIAS = "default"
+SESSION_CACHE_ALIAS = "session"
+

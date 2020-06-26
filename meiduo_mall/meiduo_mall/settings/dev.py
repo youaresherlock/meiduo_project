cdd2d6c2cde9d770be06d89db1678939f6465d2a
clarence <2263194561@qq.com>
2020-06-26 21:27:36 +0800

    配置MYSQL数据库

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -73,12 +73,17 @@ WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+# django工程默认使用的数据库sqlite3
+# 而我们要给他改为mysql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',  # 数据库主机
+        'PORT': 3306,  # 数据库端口
+        'USER': 'root',  # 数据库用户名
+        'PASSWORD': 'x1430371727',  # 数据库用户密码
+        'NAME': 'meiduo_mall'  # 数据库名字
     }
 }
 
